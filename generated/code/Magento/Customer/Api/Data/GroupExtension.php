4a74e86865efe56fbb3da7e4b0940ded0b761514
minhvuong <minhvuongnguyen1111@gmail.com>
2022-09-15 16:36:46 +0700

    Tigren_Week4

--- generated/code/Magento/Customer/Api/Data/GroupExtension.php
+++ generated/code/Magento/Customer/Api/Data/GroupExtension.php
@@ -23,4 +23,22 @@ class GroupExtension extends \Magento\Framework\Api\AbstractSimpleObject impleme
         $this->setData('exclude_website_ids', $excludeWebsiteIds);
         return $this;
     }
+
+    /**
+     * @return bool|null
+     */
+    public function getIsVisibleOnStorefront()
+    {
+        return $this->_get('is_visible_on_storefront');
+    }
+
+    /**
+     * @param bool $isVisibleOnStorefront
+     * @return $this
+     */
+    public function setIsVisibleOnStorefront($isVisibleOnStorefront)
+    {
+        $this->setData('is_visible_on_storefront', $isVisibleOnStorefront);
+        return $this;
+    }
 }
